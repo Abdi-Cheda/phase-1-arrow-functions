2237dfacd188098f6c4f36990ef7edec093fcac9
+ <abdicheda75@gmail.com>
2023-10-08 06:09:00 +0300

    Assignment Done!

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function() {
+    return 2000 / 100;
+};
+
+const square = (number) => {
+    return number * number;
+};
+
+const add = (num1, num2) => num1 + num2;
+
+module.exports = {
+    divide,
+    square,
+    add
+};
